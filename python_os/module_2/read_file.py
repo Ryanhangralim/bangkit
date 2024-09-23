a97612497c066baeda687ddd82354e811886fe9f
Ryanhangralim <ryanhangralim@gmail.com>
2024-09-23 13:16:47 +0800

    Add write file

--- python_os/module_2/read_file.py
+++ python_os/module_2/read_file.py
@@ -1,4 +1,7 @@
 file = open("example_file.txt", "r")
+print(file.readline())
+print("NEXT")
+print(file.read())
 for line in file.readlines():
     print(line, end="")
 file.close()
@@ -6,5 +9,5 @@ file.close()
 print("\n")
 
 with open("example_file.txt") as file:
-    for line in file.readlines():
-        print(line, end="")
+    for line in file:
+        print(line.strip())
---
+++ python_os/module_2/write_file.py
@@ -0,0 +1,2 @@
+with open("test_write.txt", "a") as file:
+    file.write("Testing to write a new line to a txt file")
